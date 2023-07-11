dfd5af097bad4810c5142dc15328585c1a18f755
Rayane Benamre <benamrerayane@gmail.com>
2023-07-11 18:50:27 +0100

    Removed unnecessary components from /page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,16 @@ import HeroHeader from "@/components/homepage-landing";
 import Courses from "@/components/courses";
 import Finder from "@/components/internship-finder";
 import Approach from "@/components/approach";
-import { useToast, Box } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
-  const toast = useToast();
 
   return (
-    <>
-      <Box my={["20px", "130px"]}>
-        <HeroHeader />
-
-        <Courses />
-        <Approach />
-        <Finder />
-      </Box>
-    </>
+    <Box my={["20px", "130px"]}>
+      <HeroHeader />
+      <Courses />
+      <Approach />
+      <Finder />
+    </Box>
   );
 }
