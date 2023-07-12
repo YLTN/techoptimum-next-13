fa53bb207c292484e1b10f3762129993ee163cc1
Rayane Benamre <benamrerayane@gmail.com>
2023-07-12 17:14:11 +0100

    Migrated /podcast route

---
+++ src/app/podcast/page.js
@@ -0,0 +1,20 @@
+"use client";
+import {
+  Box,
+} from "@chakra-ui/react";
+import PodcastHero from "@/components/talks-landing";
+
+export default function Podcasts() {
+
+  return (
+    <>
+      <Box mb="100px">
+
+
+        <PodcastHero />
+
+      </Box>
+
+    </>
+  );
+}
