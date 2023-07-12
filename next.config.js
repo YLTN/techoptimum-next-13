096e5de054a703585343eb3f075d2361e0e28fb2
Rayane Benamre <benamrerayane@gmail.com>
2023-07-12 17:29:01 +0100

    Migrated /privacy route

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
                 permanent: false,
                 basePath: false,
             },
+            {
+                source: "/privacy",
+                destination: "https://techoptimum.notion.site/Tech-Optimum-Privacy-Policy-6bdd940503494d458223e926381175f1?pvs=4",
+                permanent: false,
+                basePath: false,
+            }
         ];
     },
     async rewrites() {
